9b3da2f5d8dbffcd0f441929f293b04d990c36cf
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-18 12:15:40 +0900

    bmi 검사

--- Jul18_01_test/src/Omain.java
+++ Jul18_01_test/src/Omain.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Omain {
     public static void main(String[] args) {
-        int a=60,b=60,c=70;
+        Scanner input = new Scanner(System.in);
+        double cm, kg;
+        String name;
+        name = input.nextLine();
+        cm = Double.parseDouble(input.nextLine());
+        kg = Double.parseDouble(input.nextLine());
+        System.out.println(name+" is : ");
+        chk(cm,kg);
 
-        if(a>40 && b>40 && c>40 && (a+b+c/3)>=60 ){
-            System.out.println("success");
+    }
+
+    public static void chk(double height, double weight){
+        double bmi = Math.round(weight/((height/100)*(height/100)));
+        if(bmi>=30){
+            System.out.println("Extremely Obese");
+            System.out.println("BMI :" + bmi);
+        } else if (bmi>=25) {
+            System.out.println("Obese");
+            System.out.println("BMI :" + bmi);
+        } else if (bmi>= 23) {
+            System.out.println("Overweight");
+            System.out.println("BMI :" + bmi);
+        } else if (bmi>-18.5) {
+            System.out.println("normal");
+            System.out.println("BMI :" + bmi);
         }else{
-            System.out.println("fail");
+            System.out.println("under weight");
+            System.out.println("BMI :" + bmi);
         }
     }
 }
