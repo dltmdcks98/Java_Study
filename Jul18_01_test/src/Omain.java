7533357b26e658607586c1e35d112a7bf6b3e348
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-18 11:41:54 +0900

    새로운 강사님

---
+++ Jul18_01_test/src/Omain.java
@@ -0,0 +1,11 @@
+public class Omain {
+    public static void main(String[] args) {
+        int a=60,b=60,c=70;
+
+        if(a>40 && b>40 && c>40 && (a+b+c/3)>=60 ){
+            System.out.println("success");
+        }else{
+            System.out.println("fail");
+        }
+    }
+}
