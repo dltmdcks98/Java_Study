cec0428e46666f3db4d4ba027b96530e87be21b1
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-19 14:14:29 +0900

    22.07.19

---
+++ JUL_19/jul19_hasA/src/jul19_hasA/Person.java
@@ -0,0 +1,22 @@
+package jul19_hasA;
+
+public class Person {
+	String name;
+	int age;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void Print() {
+		System.out.println(name);
+		System.out.println(age);
+		
+	}
+}
---
+++ JUL_19/jul19_hasA/src/jul19_hasA/Planet.java
@@ -0,0 +1,5 @@
+package jul19_hasA;
+
+public class Planet {
+
+}
