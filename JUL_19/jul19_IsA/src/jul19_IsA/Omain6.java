59ff66d74c81ee1b78b15b876195408a552f90e7
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-19 17:53:47 +0900

    22.07.19 마무리

---
+++ JUL_19/jul19_IsA/src/jul19_IsA/Omain6.java
@@ -0,0 +1,8 @@
+package jul19_IsA;
+
+public class Omain6 {
+	public static void main(String[] args) {
+
+	}
+
+}
--- JUL_19/jul19_IsA/src/jul19_IsA/holsu.java
+++ JUL_19/jul19_IsA/src/jul19_IsA/holsu.java
@@ -3,29 +3,13 @@ package jul19_IsA;
 import java.util.Random;
 
 public class holsu extends Random {
-//	int n = nextInt(500);
 	public holsu() {
 		// TODO Auto-generated constructor stub
 	}
-	
-//	public boolean chk() {
-//		if((n%2!=0)) {
-//			return true;
-//		}else return false;
-//	}
-	
+
 	@Override
 	public int nextInt(int num) {
 		int temp = super.nextInt(num);
-		// TODO Auto-generated method stub
-		while(true) {
-			if(temp%2!=0) {
-				break;
-			}else {
-				temp = super.nextInt(num);
-			}
-		}
-		return temp;
-	 
+		return (temp%2==1)? temp : nextInt(num);
 	}
 }
