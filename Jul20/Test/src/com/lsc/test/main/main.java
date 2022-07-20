d7fcda452889b34229688188865f129bd266763b
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-20 16:51:18 +0900

    22.07.20 Test1

--- Jul20/Test/src/com/lsc/test/main/main.java
+++ Jul20/Test/src/com/lsc/test/main/main.java
@@ -12,9 +12,13 @@ public class main {
 		int num = r.nextInt(100)+1;
 		
 		int cnt =1;
-		
+		int i;
 		while(true) {
-			int i = Integer.parseInt(input.nextLine());
+			/*
+			 * nextLine과 nextInt를 같이 쓰면 버퍼에 값이 생겨서 문제가 되지만 큰 숫자가 아니라 아래와 같이 작은 숫자이면 
+			 * Int로 하는 것이 맞다.
+			 */
+			i = input.nextInt();
 			if(i>num) {
 				cnt++;
 				System.out.println("좀 더 작은 값을 입력하세요");
@@ -26,6 +30,14 @@ public class main {
 				System.out.println(num);
 				break;
 			}	
+		}
+		
+		//cmd창에서 실행시 바로 꺼지지 않도록 
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
