9c83f8788f1e0b93275691ca8b80d70867d17ea8
dltmdcks98 <dltmdcks98@gmail.com>
2022-07-22 17:41:01 +0900

    22.07.22 자전거 거리계산

--- Jul22/Jul22_6_test/src/com/lsc/jul22test/test1/Test1.java
+++ Jul22/Jul22_6_test/src/com/lsc/jul22test/test1/Test1.java
@@ -7,16 +7,21 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random ran = new Random();
-		String random[]= {"ㄱ","ㄴ","ㄷ","ㄹ","ㅁ"};
-		int rand;
+		//String은 글자가 객체로 생성되어 저장된다.
+		//char는 기본형 
+		char random[]= {'ㄱ','ㄴ','ㄷ','ㄹ','ㅁ'};
+		int rand;;
 		while(true) {
-			rand=ran.nextInt(5);
+			rand = ran.nextInt(5);
 			System.out.println(random[rand]);
-			if(random[rand].equals("ㅁ")) {
+			if(rand==4) {
 				break;
-			}
-			
+			}			
 		}
+		
+		
+		String bb= "ㄱㄴㄷㄹㅁ";
+		System.out.println(bb.charAt(4));
 	}
 
 }
